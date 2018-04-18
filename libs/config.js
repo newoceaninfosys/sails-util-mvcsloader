f80d184de68262473ac5ba3565434e1385f4bba1
Pavel Shabardin <pavel@scat.su>
2018-04-18 14:56:45 +0400

    upd sails 1.0 support

--- libs/config.js
+++ libs/config.js
@@ -2,20 +2,17 @@
  * Load config from a directory into a Sails app
  */
 
-var buildDictionary = require('sails-build-dictionary');
-var _ = require('lodash');
+var buildDictionary = require('sails-build-dictionary')
+var _ = require('lodash')
 module.exports = function (sails, dir) {
-    buildDictionary.aggregate({
-        dirname: dir,
-        exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
-        excludeDirs: /(locales|env)$/,
-        filter: /(.+)\.(js|json|coffee|litcoffee)$/,
-        identity: false
-    }, function (err, configs) {
-        sails.config = _.merge(configs, sails.config, function (a, b) {
-            if (_.isArray(a)) {
-                return a.concat(b);
-            }
-        });
-    });
-};
+  buildDictionary.aggregate({
+    dirname: dir,
+    exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
+    excludeDirs: /(locales|env)$/,
+    filter: /(.+)\.(js|json|coffee|litcoffee)$/,
+    identity: false
+  }, function (err, configs) {
+    if (err) sails.log.error(err)
+    sails.config = _.merge(configs, sails.config, (a, b) => _.isArray(a) ? a.concat(b) : undefined)
+  })
+}
--- libs/models.js
+++ libs/models.js
@@ -2,35 +2,29 @@
  * Load models from a directory into a Sails app
  */
 
-var buildDictionary = require('sails-build-dictionary');
+var buildDictionary = require('sails-build-dictionary')
 
 module.exports = function (sails, dir, cb) {
-    buildDictionary.optional({
-        dirname: dir,
-        filter: /^([^.]+)\.(js|coffee|litcoffee)$/,
-        replaceExpr: /^.*\//,
-        flattenDirectories: true
-    }, function (err, models) {
-        if (err) {
-            return cb(err);
-        }
-        
+  buildDictionary.optional({
+    dirname: dir,
+    filter: /^([^.]+)\.(js|coffee|litcoffee)$/,
+    replaceExpr: /^.*\//,
+    flattenDirectories: true
+  }, (err, models) => {
+    if (err) return cb(err)
+
         // Get any supplemental files
-        buildDictionary.optional({
-            dirname: dir,
-            filter: /(.+)\.attributes.json$/,
-            replaceExpr: /^.*\//,
-            flattenDirectories: true
-        }, function (err, supplements) {
-            if (err) {
-                return cb(err);
-            }
+    buildDictionary.optional({
+      dirname: dir,
+      filter: /(.+)\.attributes.json$/,
+      replaceExpr: /^.*\//,
+      flattenDirectories: true
+    }, (err, supplements) => {
+      if (err) return cb(err)
+      let finalModels = {...models, supplements} || {}
+      sails.models = {...finalModels, ...sails.models}
 
-            var finalModels = sails.util.merge(models, supplements);
-            sails.hooks.orm.models = sails.util.merge(finalModels || {}, sails.hooks.orm.models || {});
-            sails.models = sails.util.merge(finalModels || {}, sails.models || {});
-
-            cb();
-        });
-    });
-};
+      cb()
+    })
+  })
+}
