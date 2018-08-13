e985aec606ef5df649129c2b726e37b4fecc8905
Pavel Shabardin <pavel@scat.su>
2018-08-13 11:00:25 +0400

    [fix] Config import. Build a dictionary of named modules

--- libs/config.js
+++ libs/config.js
@@ -2,17 +2,17 @@
  * Load config from a directory into a Sails app
  */
 
-var buildDictionary = require('sails-build-dictionary')
+var includeAll = require('include-all')
 var _ = require('lodash')
 module.exports = function (sails, dir) {
-  buildDictionary.aggregate({
+  includeAll.optional({
     dirname: dir,
     exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
     excludeDirs: /(locales|env)$/,
-    filter: /(.+)\.(js|json|coffee|litcoffee)$/,
-    identity: false
+    filter: /(.+)\.(js|json|coffee|litcoffee)$/
   }, function (err, configs) {
     if (err) sails.log.error(err)
     sails.config = _.merge(configs, sails.config, (a, b) => _.isArray(a) ? a.concat(b) : undefined)
   })
 }
+
